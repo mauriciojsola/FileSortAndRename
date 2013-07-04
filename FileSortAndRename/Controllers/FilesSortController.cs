4817864785725e24124c3e4352157562df1606e8
mauriciojsola <mauriciojsola11@gmail.com>
2013-07-04 14:12:15 -0300

    Added sorting an renaming functionality.

--- FileSortAndRename/Controllers/FilesSortController.cs
+++ FileSortAndRename/Controllers/FilesSortController.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Web.Mvc;
 using FileSortAndRename.Models;
 
@@ -33,5 +35,51 @@ namespace FileSortAndRename.Controllers
             return View(fileList);
         }
 
+        public ActionResult SaveFiles(string filesList)
+        {
+            var orderIndex = 1;
+            var files = filesList.Split('|').ToList();
+
+            // Get the files from the OrderedFiles directory
+            var oldOrderedFiles = Directory.GetFiles(Server.MapPath("~/Uploads/OrderedFiles"));
+
+            try
+            {
+                foreach (var file in files)
+                {
+                    var sourceFilePath = Path.Combine(Server.MapPath("~/Uploads"), file);
+                    if (System.IO.File.Exists(sourceFilePath))
+                    {
+                        // Create the target file path
+                        var destFilePath = Path.Combine(Server.MapPath("~/Uploads/OrderedFiles"), string.Format("{0}_{1}", orderIndex.ToString().PadLeft(3, '0'), file));
+
+                        DeleteOldFiles(oldOrderedFiles, file);
+                        
+                        // Copy the new ordered file
+                        System.IO.File.Copy(sourceFilePath, destFilePath);
+                    }
+                    orderIndex++;
+                }
+
+            }
+            catch (Exception)
+            {
+                return Json(false, JsonRequestBehavior.AllowGet);
+            }
+            return Json(true, JsonRequestBehavior.AllowGet);
+        }
+
+        private void DeleteOldFiles(string[] oldOrderedFiles, string file)
+        {
+            var oldFiles = oldOrderedFiles.Where(x => x.Contains(file)).ToList();
+
+            foreach (var oldFile in oldFiles)
+            {
+                if (System.IO.File.Exists(oldFile))
+                {
+                    System.IO.File.Delete(oldFile);
+                }
+            }
+        }
     }
 }
